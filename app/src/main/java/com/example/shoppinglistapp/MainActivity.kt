96a60b870e5509368906dfcda1c9a67c976581ee
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-10 10:22:43 +0700

    refactored app

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -3,18 +3,6 @@ package com.example.shoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,41 +15,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun ShoppingListApp() {
-
-    val sItems = remember { mutableStateListOf<ShoppingItem>() }
-
-    sItems.add(ShoppingItem(0, "Flower", 1))
-    sItems.add(ShoppingItem(1, "Lamborghini", 3))
-    sItems.add(ShoppingItem(2, "Mouse", 7))
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-    ) {
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-        ) {
-            Text("Add item")
-        }
-
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            items(sItems) { item ->
-                Text("${item.name} = ${item.quantity}")
-            }
-        }
-    }
-}
-
-data class ShoppingItem(
-    val id : Int,
-    var name : String,
-    var quantity : Int,
-    var isEditing : Boolean = false,
-)
---
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingListApp.kt
@@ -0,0 +1,52 @@
+package com.example.shoppinglistapp
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+data class ShoppingItem(
+    val id : Int,
+    var name : String,
+    var quantity : Int,
+    var isEditing : Boolean = false,
+)
+
+@Composable
+fun ShoppingListApp() {
+
+    val sItems = remember { mutableStateListOf<ShoppingItem>() }
+
+    sItems.add(ShoppingItem(0, "Flower", 1))
+    sItems.add(ShoppingItem(1, "Lamborghini", 3))
+    sItems.add(ShoppingItem(2, "Mouse", 7))
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+        ) {
+            Text("Add item")
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            items(sItems) { item ->
+                Text("${item.name} = ${item.quantity}")
+            }
+        }
+    }
+}
