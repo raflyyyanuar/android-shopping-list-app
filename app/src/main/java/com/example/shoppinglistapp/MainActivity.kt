701d2e0e6dd66ffbf6b35ab5db3d54b34bf6ddaa
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-10 10:20:16 +0700

    implemented ShoppingItem data class

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -3,17 +3,18 @@ package com.example.shoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,6 +30,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShoppingListApp() {
+
+    val sItems = remember { mutableStateListOf<ShoppingItem>() }
+
+    sItems.add(ShoppingItem(0, "Flower", 1))
+    sItems.add(ShoppingItem(1, "Lamborghini", 3))
+    sItems.add(ShoppingItem(2, "Mouse", 7))
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -39,5 +47,21 @@ fun ShoppingListApp() {
         ) {
             Text("Add item")
         }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            items(sItems) { item ->
+                Text("${item.name} = ${item.quantity}")
+            }
+        }
     }
 }
+
+data class ShoppingItem(
+    val id : Int,
+    var name : String,
+    var quantity : Int,
+    var isEditing : Boolean = false,
+)
